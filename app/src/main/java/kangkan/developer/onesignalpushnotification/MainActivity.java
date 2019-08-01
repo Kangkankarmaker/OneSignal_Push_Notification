ade4e4c650e8e43416423769a6549d1076779ef6
kangkan <kangkankarmaker00@gmail.com>
2019-08-01 20:39:20 +0600

    Initial commit

---
+++ app/src/main/java/kangkan/developer/onesignalpushnotification/MainActivity.java
@@ -0,0 +1,19 @@
+package kangkan.developer.onesignalpushnotification;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    MyApplication myApplication;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myApplication = MyApplication.getmInstance();
+
+
+    }
+}
